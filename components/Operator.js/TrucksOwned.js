3736f62993eb3243be8ff9b30424875db70c91b4
Justin Sue <justincsue@gmail.com>
2020-02-28 18:39:40 -0800

    first save

---
+++ components/Operator.js/TrucksOwned.js
@@ -0,0 +1,4 @@
+//array of trucks operator owns or runs
+//Operators need access to create, view and update and delete truck object
+//have image of trucks img or url, cuisin type, customer rating, customerRatingAvg
+//need a customer rating array to give customer rating and be able to update rating as well
